661d49df89a1b9d7f3ae1773b186cf6d9884d67d
s2pdeveloper <124123282+s2pdeveloper@users.noreply.github.com>
2024-05-19 11:28:31 +0530

    changes

--- bashFile.sh
+++ bashFile.sh
@@ -6,10 +6,3 @@ whoami >> output.txt;
 cat output.txt;
 EOF
 
-MAIL_USERNAME=poojadabi1804@gmail.com
-MAIL_PASSWORD=vncl bggr ovpe dnrk
-MAIL_FROM=poojadabi1804@gmail.com
-MAIL_PORT=587
-MAIL_SERVER=smtp.gmail.com
-MAIL_FROM_NAME=pooja dabi
-SECRET_KEY=autodeployhelpes
